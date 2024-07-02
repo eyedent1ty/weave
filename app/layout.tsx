de3f27b8e6496b8380a9498082b4eeb4278fc115
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-07-02 23:08:17 +0800

    installed clerkjs and created a middleware to protect routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
 import './globals.css';
 
 import StoreProvider from './StoreProvider';
@@ -24,22 +25,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <StoreProvider>
-      <html lang="en">
-        <body className={`${inter.className} relative`}>
-          <div className="relative flex justify-center bg-tertiary text-secondary">
-            <Nav />
-            <main className="sm:mx-[76px] sm:max-w-[600px] pt-2 w-screen min-h-[calc(100vh)] sm:rounded-t-3xl sm:border border-border-color bg-primary text-secondary sm:mt-14 shadow-md">
-              {children}
-            </main>
-          </div>
-          <FloatingButton />
-          <PostDialog open={true} />
-          <Backdrop />
-          <ReplyDialog open={true} />
-          <EditProfileDialog open={true} />
-        </body>
-      </html>
-    </StoreProvider>
+    <ClerkProvider>
+      <StoreProvider>
+        <html lang="en">
+          <body className={`${inter.className} relative`}>
+            <div className="relative flex justify-center bg-tertiary text-secondary">
+              <Nav />
+              <main className="sm:mx-[76px] sm:max-w-[600px] pt-2 w-screen min-h-[calc(100vh)] sm:rounded-t-3xl sm:border border-border-color bg-primary text-secondary sm:mt-14 shadow-md">
+                {children}
+              </main>
+            </div>
+            <FloatingButton />
+            <PostDialog open={true} />
+            <Backdrop />
+            <ReplyDialog open={true} />
+            <EditProfileDialog open={true} />
+          </body>
+        </html>
+      </StoreProvider>
+    </ClerkProvider>
   );
 }
