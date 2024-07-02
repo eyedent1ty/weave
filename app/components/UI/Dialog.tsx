01ac9cc6c7aa1aef9d039f22f2396b58947bb43c
John Daniel Del Monte <delmontejohndaniel80@gmail.com>
2024-07-02 17:41:01 +0800

    added framer-motion to the project

--- app/components/UI/Dialog.tsx
+++ app/components/UI/Dialog.tsx
@@ -1,4 +1,8 @@
-import type { FC, ReactNode, Ref, MutableRefObject } from 'react';
+'use client';
+
+import { motion } from 'framer-motion';
+import { useState, useEffect } from 'react';
+import type { ReactNode, Ref } from 'react';
 import { forwardRef } from 'react';
 
 interface DialogInterface {
@@ -13,10 +17,38 @@ const Dialog = forwardRef<HTMLDialogElement, DialogInterface>(
     { headerContent, mainContent, footerContent, open },
     ref: Ref<HTMLDialogElement>
   ) => {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+      const handleResize = () => {
+        setWidth(window.innerWidth);
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const isMobile = width < 700;
+
     return (
-      <dialog
+      <motion.dialog
+        initial={{
+          opacity: 0,
+          transform: `${
+            isMobile
+              ? 'translate(0, 100%)'
+              : 'translate(-50%, calc(100px - 50%))'
+          }`
+        }}
+        animate={{
+          opacity: 1,
+          transform: `${
+            isMobile ? 'translate(0, 0)' : 'translate(-50%, -50%)'
+          }`
+        }}
         open={open}
-        className="fixed max-sm:h-screen max-sm:w-screen max-sm:top-0 flex flex-col justify-between mx-0 border border-border-color bg-primary text-secondary shadow-md pl-6 pt-6 pb-4 z-50 sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2 sm:w-[580px] sm:max-h-[520px] sm:rounded-3xl"
+        className="fixed max-sm:h-screen max-sm:w-screen max-sm:top-0 flex flex-col justify-between mx-0 border border-border-color bg-primary text-secondary shadow-md pl-6 pt-6 pb-4 z-50 sm:w-[580px] sm:max-h-[520px] sm:rounded-3xl sm:top-1/2 sm:left-1/2 sm:-translate-x-1/2 sm:-translate-y-1/2"
         ref={ref}
       >
         <header className="mb-6 sm:hidden">{headerContent}</header>
@@ -24,7 +56,7 @@ const Dialog = forwardRef<HTMLDialogElement, DialogInterface>(
           {mainContent}
         </main>
         <footer className="h-[84px] p-6 bg-primary">{footerContent}</footer>
-      </dialog>
+      </motion.dialog>
     );
   }
 );
